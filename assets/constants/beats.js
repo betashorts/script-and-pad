5344ac675d60ad496cb5f20bed37a42f423d3d38
betashorts <betashorts@users.noreply.github.com>
2025-04-27 10:17:44 +0000

    deploy: 107f2bda649f53229efa78f0efc4488c52c1d38e

--- assets/constants/beats.js
+++ assets/constants/beats.js
@@ -1,5 +1,5 @@
 // Beat categories and their associated MIDI files
-export const BEAT_CATEGORIES = {
+window.BEAT_CATEGORIES = {
   // "Basic Beats": [
   //   {
   //     name: "Basic Rock Beat",
@@ -180,11 +180,11 @@ export const BEAT_CATEGORIES = {
 };
 
 // Helper function to get all beats in a flat array
-export function getAllBeats(formatBeatName) {
-  return Object.values(BEAT_CATEGORIES)
+window.getAllBeats = function (formatBeatName) {
+  return Object.values(window.BEAT_CATEGORIES)
     .flat()
     .map((beat) => ({
       ...beat,
       name: formatBeatName ? formatBeatName(beat.file) : beat.name,
     }));
-}
+};
