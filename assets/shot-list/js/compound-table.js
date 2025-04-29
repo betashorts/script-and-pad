15fe7ebd41913ef59f3ea4314d2a3b5b25830857
danny1547 <dineshiit1998@gmail.com>
2025-04-29 17:27:59 +0530

    STABLE COMMIT COMPOUND TABLE IS FINE

--- assets/shot-list/js/compound-table.js
+++ assets/shot-list/js/compound-table.js
@@ -1,5 +1,5 @@
 // Debug version 17
-console.log("Compound Table deployed - version 21");
+console.log("Compound Table deployed - version 22");
 
 // Compound Table Data Structure Example
 let compoundTableData = {
