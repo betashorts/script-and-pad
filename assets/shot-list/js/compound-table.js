32c967d3b96a8d5c8e1caa10da233d265edd5d90
danny1547 <dineshiit1998@gmail.com>
2025-04-29 12:36:33 +0530

    fixing L4 horinzontal stacking

--- assets/shot-list/js/compound-table.js
+++ assets/shot-list/js/compound-table.js
@@ -1,5 +1,5 @@
-// Debug version 11
-console.log("Compound Table deployed - version 11");
+// Debug version 12
+console.log("Compound Table deployed - version 12");
 
 // Compound Table Data Structure Example
 let compoundTableData = {
