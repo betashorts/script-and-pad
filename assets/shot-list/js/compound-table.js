e31ce9a77820d295f28e0f8f527ca72cc175140a
betashorts <betashorts@users.noreply.github.com>
2025-04-30 05:33:49 +0000

    deploy: 86d41b956a9aab2f5d3c0db638a4eca66c094bc5

--- assets/shot-list/js/compound-table.js
+++ assets/shot-list/js/compound-table.js
@@ -1,5 +1,5 @@
 // Debug version 17
-console.log("Compound Table deployed - version 25");
+console.log("Compound Table deployed - version 26");
 
 // Compound Table Data Structure Example
 let compoundTableData = {
